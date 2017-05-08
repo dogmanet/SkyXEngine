e5933b86ae06912ffa5e8b3a99f85a5a53e0afc7
D-AIRY <admin@ds-servers.com>
2017-05-08 08:43:44 +0300

    Добавлено исполнение пользовательского конфигурационного файла, у каждого может быть свой

--- source/skyxengine.cpp
+++ source/skyxengine.cpp
@@ -90,6 +90,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLin
 	//!@TODO: Найти для этого более подходящее место
 	Core_0RegisterCVarFloat("cl_mousesense", 0.001f, "Mouse sense value");
 
+	Core_0ConsoleExecCmd("exec ../userconfig.cfg");
+
 	Level::Load("stalker_atp");
 
 	IAnimPlayer * pl;
