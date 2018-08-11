49327ef916a5a9d3ea37c1f5ca50e182046935f3
D-AIRY <admin@ds-servers.com>
2018-08-12 00:27:18 +0300

    Logic relay entity

---
+++ source/game/LogicRelay.cpp
@@ -0,0 +1,64 @@
+
+/***********************************************************
+Copyright © Vitaliy Buturlin, Evgeny Danilovich, 2017, 2018
+See the license in LICENSE
+***********************************************************/
+
+#include "LogicRelay.h"
+
+/*! \skydocent logic_relay
+Логическое реле
+*/
+
+BEGIN_PROPTABLE(CLogicRelay)
+	//! Включить
+	DEFINE_INPUT(turnOn, "turnOn", "Turn On", PDF_NONE)
+	//! Выключить
+	DEFINE_INPUT(turnOff, "turnOff", "Turn Off", PDF_NONE)
+	//! Переключить
+	DEFINE_INPUT(trigger, "toggle", "Toggle", PDF_NONE)
+	//! Активировать
+	DEFINE_INPUT(trigger, "trigger", "Trigger", PDF_NONE)
+
+	//! При активации
+	DEFINE_OUTPUT(m_onTrigger, "OnTrigger", "On Trigger")
+
+	//! Изначально выключен
+	DEFINE_FLAG(LOGIC_START_DISABLED, "Start Disabled")
+END_PROPTABLE()
+
+REGISTER_ENTITY(CLogicRelay, logic_relay);
+
+CLogicRelay::CLogicRelay(CEntityManager * pMgr):
+	BaseClass(pMgr)
+{
+	m_isEnabled = true;
+}
+
+void CLogicRelay::turnOn(inputdata_t * pInputdata)
+{
+	m_isEnabled = true;
+}
+
+void CLogicRelay::turnOff(inputdata_t * pInputdata)
+{
+	m_isEnabled = false;
+}
+
+void CLogicRelay::toggle(inputdata_t * pInputdata)
+{
+	m_isEnabled = !m_isEnabled;
+}
+
+void CLogicRelay::trigger(inputdata_t * pInputdata)
+{
+	if(m_isEnabled)
+	{
+		FIRE_OUTPUT(m_onTrigger, pInputdata->pInflictor);
+	}
+}
+
+void CLogicRelay::onPostLoad()
+{
+	m_isEnabled = !(getFlags() & LOGIC_START_DISABLED);
+}
---
+++ source/game/LogicRelay.h
@@ -0,0 +1,42 @@
+
+/***********************************************************
+Copyright © Vitaliy Buturlin, Evgeny Danilovich, 2017, 2018
+See the license in LICENSE
+***********************************************************/
+
+/*!
+\file
+Логическое реле
+*/
+
+#ifndef __LOGIC_RELAY_H
+#define __LOGIC_RELAY_H
+
+#include "PointEntity.h"
+
+#define LOGIC_START_DISABLED 0x00010000
+
+/*! Логическое реле
+\ingroup cpointentity
+*/
+class CLogicRelay: public CPointEntity
+{
+	DECLARE_CLASS(CLogicRelay, CPointEntity);
+	DECLARE_PROPTABLE();
+public:
+	DECLARE_CONSTRUCTOR();
+	
+protected:
+	void turnOn(inputdata_t * pInputdata);
+	void turnOff(inputdata_t * pInputdata);
+	void trigger(inputdata_t * pInputdata);
+	void toggle(inputdata_t * pInputdata);
+
+	void onPostLoad();
+
+	output_t m_onTrigger;
+
+	bool m_isEnabled;
+};
+
+#endif
