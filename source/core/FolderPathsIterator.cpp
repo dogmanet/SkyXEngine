b26ce44fc99b8fa5197a65e334389adf62be9a3c
Dokunov <vanya619@list.ru>
2019-07-27 17:57:18 +0300

    Add FolderPathsIterator

---
+++ source/core/FolderPathsIterator.cpp
@@ -0,0 +1,61 @@
+#include "FolderPathsIterator.h"
+
+CFolderPathsIterator::CFolderPathsIterator(Array<String> *paths)
+: m_paths(paths)
+{
+
+}
+
+const char *CFolderPathsIterator::next()
+{
+    WIN32_FIND_DATA FindFileData;
+    HANDLE hf;
+
+    int size = m_paths->size();
+
+    while (index < size)
+    {
+        hf = INVALID_OR_NULL(m_handle) ? FindFirstFile(((*m_paths)[index] + "*.*").c_str(), &FindFileData) : m_handle;
+
+        if (hf != INVALID_HANDLE_VALUE)
+        {
+            while (FindNextFile(hf, &FindFileData) != 0)
+            {
+                m_handle = hf;
+
+                m_pathStr = (*m_paths)[index] + FindFileData.cFileName;
+
+                DWORD flag = GetFileAttributes(m_pathStr.c_str());
+
+                if (!strcmp(FindFileData.cFileName, "..") || !strcmp(FindFileData.cFileName, "."))
+                {
+                    continue;
+                }
+
+                //Берет только имена директорий
+                if (flag != INVALID_FILE_ATTRIBUTES && flag & FILE_ATTRIBUTE_DIRECTORY)
+                {
+                    //Возвращаем полный путь к директории
+                    return m_pathStr.c_str();
+                }
+            }
+            FIND_CLOSE(m_handle);
+            ++index;
+        }
+    }
+
+    //Если вообще не нашли файлов возвращаем nullptr
+    return nullptr;
+}
+
+void CFolderPathsIterator::reset()
+{
+    index = 0;
+    FIND_CLOSE(m_handle);
+}
+
+CFolderPathsIterator::~CFolderPathsIterator()
+{
+    FIND_CLOSE(m_handle);
+    mem_delete_a(m_paths);
+}
---
+++ source/core/FolderPathsIterator.h
@@ -0,0 +1,32 @@
+/***********************************************************
+Copyright © Vitaliy Buturlin, Evgeny Danilovich, Ivan Dokunov, 2019
+See the license in LICENSE
+***********************************************************/
+
+#ifndef __FOLDER_PATHS_ITERATOR_H
+#define __FOLDER_PATHS_ITERATOR_H
+
+#include "FileSystem.h"
+
+class CFolderPathsIterator final : public IFileSystem::IFileIterator
+{
+private:
+
+    Array<String>* m_paths;
+    String m_pathStr;
+
+    int index = 0;
+
+    HANDLE m_handle = nullptr;
+
+public:
+    CFolderPathsIterator(Array<String> *paths);
+
+    const char *XMETHODCALLTYPE next() override;
+
+    void XMETHODCALLTYPE reset() override;
+
+    ~CFolderPathsIterator();
+};
+
+#endif
