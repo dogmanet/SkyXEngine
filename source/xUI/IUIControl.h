cc615eebb30f56dbddb377797e6683a911299d73
Dokunov <vanya619@list.ru>
2019-12-14 01:54:34 +0300

    Added methods in IUIControl interface

--- source/xUI/IUIControl.h
+++ source/xUI/IUIControl.h
@@ -6,16 +6,22 @@
 class IUIControl : public IXUnknown
 {
 	//! Метод установки контента в контроле
-	virtual void XMETHODCALLTYPE setTitle(const char *szTitle) = 0;
+	virtual void XMETHODCALLTYPE setLabel(const char *szTitle) = 0;
 
 	//! Метод установки позиции в окне
 	virtual void XMETHODCALLTYPE setPosition(int iPosX, int iPosY) = 0;
 
 	//! Метод установки видимости для контрола
-	virtual void XMETHODCALLTYPE setVisible(bool visible) = 0;
+	virtual void XMETHODCALLTYPE setVisible(bool bVisible) = 0;
+
+	//! Метод установки размера по высоте и ширине
+	virtual void XMETHODCALLTYPE setSize(int iSizeX, int iSizeY) = 0;
 
 	//! Метод возвращает видимость контрола
 	virtual bool XMETHODCALLTYPE isVisible() = 0;
+
+	//! Метод устанавливает имя элемента
+	virtual bool XMETHODCALLTYPE setName(const char *szName) = 0;
 };
 
 #endif
